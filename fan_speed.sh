ab93f773239b30c73da1de8f727b42da26b6b621
tclela20 <70184188+tclela20@users.noreply.github.com>
2021-06-07 22:14:54 -0500

    Update fan_speed.sh
    Takes manual control of the fan speeds on the Dell PowerEdge R410 server. After taking control the script manually lowers the speed to the percentage specified.

--- fan_speed.sh
+++ fan_speed.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "Taking manual control of R410 Fans on (machine_name)..."
-ipmitool -I lanplus -H 192.168.4.19 -U (user) -P (password) raw 0x30 0x30 0x01 0x00
-echo "Manual Controls Engaged..."
-echo "Setting Fan Speeds to 40%..."
-ipmitool -I lanplus -H 192.168.4.19 -U (user) -P (password) raw 0x30 0x30 0x02 0xff 0x28
-echo "Complete, R410 Fans set!"
+# Need to find the hexadecimal use printf '%x\n' 40 and update 40 to what you want.
+# After the printf command update the 2nd to last line and change 0x28 to 0x plus the output of printf.
 
+host = ""
+user = ""
+pword = ""
+
+echo "Taking manual control of R410 Fans on (machine_name)..."
+echo ipmitool -I lanplus -H $host -U $user -P $pword raw 0x30 0x30 0x01 0x00
+echo "Manual Controls Engaged..."
+echo "Setting Fan Speeds to 50%..."
+echo ipmitool -I lanplus -H $host -U $user -P $pword raw 0x30 0x30 0x02 0xff 0x32
